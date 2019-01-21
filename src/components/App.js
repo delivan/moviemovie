2158e7955c01a803bf4e5389631687db7974ead0
hyuk22 <wjdgurdl272@gmail.com>
2019-01-21 23:55:38 +0900

    Initialize react router

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Router from "components/Router";
+import Header from "components/Header";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <Router />
+      </>
+    );
+  }
+}
+
+export default App;
